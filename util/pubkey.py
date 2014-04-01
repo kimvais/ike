290d8249680eb4b4d1316fa75648dc60d241330e
Kimmo Parviainen-Jalanko <kimvais@ssh.com>
2014-04-01 14:11:34 +0300

    Added RSA signing interface + a test key

---
+++ util/pubkey.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2014 Kimmo Parviainen-Jalanko
+
+import rsa
+
+def sign(data, filename, hash_alg="SHA-256"):
+    assert isinstance(data, bytes)
+    with open(filename, 'rb') as keyfile:
+        private_key = rsa.PrivateKey.load_pkcs1(keyfile.read())
+    return rsa.sign(data, private_key, hash_alg)
+
+
+def verify(data, signature, filename):
+    with open(filename, 'rb') as publicfile:
+        public_key = rsa.PublicKey.load_pkcs1(publicfile.read())
+    return rsa.verify(data, signature, public_key)
